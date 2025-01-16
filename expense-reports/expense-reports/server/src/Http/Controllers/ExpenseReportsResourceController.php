900880b3ebdee00536e499d59f59448aae4028b5
acsdeveloper <developer@agilexcyber.in>
2025-01-16 13:00:10 +0530

    Add files via upload

---
+++ expense-reports/expense-reports/server/src/Http/Controllers/ExpenseReportsResourceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fleetbase\ExpenseReports\Http\Controllers;
+
+use Fleetbase\Http\Controllers\FleetbaseController;
+
+class ExpenseReportsResourceController extends FleetbaseController
+{
+    /**
+     * The package namespace used to resolve from.
+     */
+    public string $namespace = '\Fleetbase\ExpenseReports';
+}
